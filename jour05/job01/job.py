e6261139cc916e03bfeb3fc7bb31e326466112b0
sami-brassier <115148513+sami-brassier@users.noreply.github.com>
2023-03-31 16:48:12 +0200

    Delete jour05 directory

--- jour05/job01/job.py
+++
@@ -1,8 +0,0 @@
-def factorielle(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorielle(n-1)
-
-nombre = int(input("Entrez un nombre entier : "))
-print("La factorielle de", nombre, "est", factorielle(nombre))
--- jour05/job03/job.py
+++
@@ -1,9 +0,0 @@
-def longueur(chaine):
-    if chaine == '':
-        return 0
-    else:
-        return 1 + longueur(chaine[1:])
-
-chaine = "Hello, world!"
-longueur_chaine = longueur(chaine)
-print("La longueur de la chaîne", chaine, "est", longueur_chaine)
